fc78d2c32cf37bfe31259c3823b74112c8776806
Stefan Kendall <stefankendall@gmail.com>
2016-10-19 09:46:07 -0400

    Remove dist

--- dist/scripts/main.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("app",[]),angular.module("app").controller("RoleController",["$scope",function(o){var a=this;$.getJSON("config.json",function(l){a.data=l,o.$apply()})}]);
